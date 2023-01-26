f58f04493fb00252dd69e9e208aaf93ad4d1055a
Lars Grammel <lars.grammel@gmail.com>
2023-01-26 11:33:28 +0100

    Add extension module under lib/

---
+++ lib/vscode-extension/src/extension.ts
@@ -0,0 +1,7 @@
+import * as vscode from "vscode";
+
+export const activate = async (context: vscode.ExtensionContext) => {
+    console.log("Hello, World!");
+};
+
+export const deactivate = async () => {};
