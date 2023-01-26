ade64cc55543c2e850f5c33726334532d4683c92
Lars Grammel <lars.grammel@gmail.com>
2023-01-26 12:37:46 +0100

    Add commands to store/clear the Open AI API key.

---
+++ lib/vscode-extension/src/ApiKeyManager.ts
@@ -0,0 +1,43 @@
+/*
+ * Copyright P42 Software UG (haftungsbeschränkt). All Rights Reserved.
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ */
+
+import * as vscode from 'vscode';
+
+const OPEN_AI_API_KEY_SECRET_KEY = 'rubberduck.openAI.apiKey';
+
+export class ApiKeyManager {
+  private readonly secretStorage: vscode.SecretStorage;
+
+  constructor({ secretStorage }: { secretStorage: vscode.SecretStorage }) {
+    this.secretStorage = secretStorage;
+  }
+
+  async clearOpenAIApiKey(): Promise<void> {
+    await this.secretStorage.delete(OPEN_AI_API_KEY_SECRET_KEY);    
+  }
+
+  private async storeApiKey(apiKey: string): Promise<void> {
+    return this.secretStorage.store(OPEN_AI_API_KEY_SECRET_KEY, apiKey);
+  }
+
+  async enterOpenAIApiKey() {
+    await this.clearOpenAIApiKey();
+
+    const apiKey = await vscode.window.showInputBox({
+      title: 'Enter your Open AI API key',
+      ignoreFocusOut: true,
+      placeHolder: 'Open AI API key',
+    });
+
+    if (apiKey == null) {
+      return; // user aborted input
+    }
+
+    await this.storeApiKey(apiKey);
+
+    vscode.window.showInformationMessage('OpenAI API key stored.');
+  }
+}
--- lib/vscode-extension/src/extension.ts
+++ lib/vscode-extension/src/extension.ts
@@ -1,7 +1,27 @@
 import * as vscode from "vscode";
+import { ApiKeyManager } from "./ApiKeyManager";
 
 export const activate = async (context: vscode.ExtensionContext) => {
-    console.log("Hello, World!");
+  const apiKeyManager = new ApiKeyManager({
+    secretStorage: context.secrets,
+  });
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "rubberduck.enterOpenAIApiKey",
+      apiKeyManager.enterOpenAIApiKey.bind(apiKeyManager)
+    )
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "rubberduck.clearOpenAIApiKey",
+      async () => {
+        await apiKeyManager.clearOpenAIApiKey();
+        vscode.window.showInformationMessage('OpenAI API key cleared.');
+      }
+    )
+  );
 };
 
 export const deactivate = async () => {};
