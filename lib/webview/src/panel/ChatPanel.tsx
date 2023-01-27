ba3f5ea5822e98547eda408616a6d7cc8cb20fce
Lars Grammel <lars.grammel@gmail.com>
2023-01-27 19:37:39 +0100

    Extract ChatPanel component.

---
+++ lib/webview/src/panel/ChatPanel.tsx
@@ -0,0 +1,42 @@
+import { PanelState } from "@rubberduck/common";
+import React from "react";
+import { CollapsedConversationView } from "../component/CollapsedConversationView";
+import { ExpandedConversationView } from "../component/ExpandedConversationView";
+import { SendMessage } from "../vscode/SendMessage";
+
+export const ChatPanel: React.FC<{
+  sendMessage: SendMessage;
+  panelState: PanelState;
+}> = ({ panelState, sendMessage }) => {
+  if (panelState === undefined) {
+    return <></>;
+  }
+
+  return (
+    <div>
+      {panelState.conversations.map((conversation, i) =>
+        panelState.selectedConversationIndex === i ? (
+          <ExpandedConversationView
+            conversation={conversation}
+            onSendMessage={(message: string) =>
+              sendMessage({
+                type: "sendChatMessage",
+                data: { index: i, message },
+              })
+            }
+          />
+        ) : (
+          <CollapsedConversationView
+            conversation={conversation}
+            onClick={() =>
+              sendMessage({
+                type: "clickCollapsedExplanation",
+                data: { index: i },
+              })
+            }
+          />
+        )
+      )}
+    </div>
+  );
+};
--- lib/webview/src/webview.tsx
+++ lib/webview/src/webview.tsx
@@ -1,8 +1,7 @@
 import { PanelState } from "@rubberduck/common";
 import * as React from "react";
 import { createRoot } from "react-dom/client";
-import { CollapsedConversationView } from "./component/CollapsedConversationView";
-import { ExpandedConversationView } from "./component/ExpandedConversationView";
+import { ChatPanel } from "./panel/ChatPanel";
 import { sendMessage } from "./vscode/SendMessage";
 import * as StateManager from "./vscode/StateManager";
 
@@ -15,33 +14,7 @@ if (rootElement != undefined) {
     try {
       reactRoot.render(
         <React.StrictMode>
-          {panelState && (
-            <div>
-              {panelState.conversations.map((conversation, i) =>
-                panelState.selectedConversationIndex === i ? (
-                  <ExpandedConversationView
-                    conversation={conversation}
-                    onSendMessage={(message: string) =>
-                      sendMessage({
-                        type: "sendChatMessage",
-                        data: { index: i, message },
-                      })
-                    }
-                  />
-                ) : (
-                  <CollapsedConversationView
-                    conversation={conversation}
-                    onClick={() =>
-                      sendMessage({
-                        type: "clickCollapsedExplanation",
-                        data: { index: i },
-                      })
-                    }
-                  />
-                )
-              )}
-            </div>
-          )}
+          <ChatPanel sendMessage={sendMessage} panelState={panelState} />
         </React.StrictMode>
       );
     } catch (error) {
