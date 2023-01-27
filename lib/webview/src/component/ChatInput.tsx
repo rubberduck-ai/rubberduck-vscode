11b99e32f9242b5ed60b17681f28736ac2b117cd
Lars Grammel <lars.grammel@gmail.com>
2023-01-27 18:43:47 +0100

    Activate input box automatically.

--- lib/webview/src/component/ChatInput.tsx
+++ lib/webview/src/component/ChatInput.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 export const ChatInput: React.FC<{
   placeholder?: string;
   onSend: (message: string) => void;
 }> = ({ placeholder, onSend }) => {
+  // callback to automatically focus the input box
+  const callbackRef = useCallback((inputElement: HTMLTextAreaElement) => {
+    if (inputElement) {
+      // delay focus (auto-focussing the input box did not work otherwise)
+      setTimeout(() => {
+        inputElement.focus();
+      }, 50);
+    }
+  }, []);
+
   return (
     <div className="chat-input">
       <textarea
+        ref={callbackRef}
         placeholder={placeholder}
         rows={1 /* TODO auto-expand */}
         onKeyUp={(e) => {
