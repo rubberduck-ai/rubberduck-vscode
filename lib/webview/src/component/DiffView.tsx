04837b16e50a8d6fa26d9d1a4675dda2ed18cff9
Lars Grammel <lars.grammel@gmail.com>
2023-01-29 18:55:38 +0100

    Use side-by-side diff.

--- lib/webview/src/component/DiffView.tsx
+++ lib/webview/src/component/DiffView.tsx
@@ -26,7 +26,7 @@ export const DiffView: React.FC<DiffViewProps> = ({ diff }) => {
         }) => (
           <Diff
             key={`${oldRevision}-${newRevision}-unified`}
-            viewType="unified"
+            // viewType="unified"
             diffType={type}
             hunks={hunks}
           >
