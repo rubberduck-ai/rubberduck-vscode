f87372124a3e651d75d41ca447ae610b045f17fd
Lars Grammel <lars.grammel@gmail.com>
2023-01-29 17:17:44 +0100

    Store editor state to prevent editor from going blank when it's moved.

--- lib/webview/src/webview.tsx
+++ lib/webview/src/webview.tsx
@@ -1,54 +1,47 @@
 import { webviewApi } from "@rubberduck/common";
 import * as React from "react";
-import { createRoot, Root } from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { ChatPanelView } from "./panel/ChatPanelView";
 import { DiffPanelView } from "./panel/DiffPanelView";
 import { sendMessage } from "./vscode/SendMessage";
-
-let reactRoot: Root | undefined = undefined;
-
-const render = (panelState?: webviewApi.PanelState) => {
-  try {
-    reactRoot?.render(
-      <React.StrictMode>
-        {(() => {
-          switch (panelState?.type) {
-            case "chat":
-              return (
-                <ChatPanelView
-                  sendMessage={sendMessage}
-                  panelState={panelState}
-                />
-              );
-            case "diff":
-              return (
-                <DiffPanelView
-                  sendMessage={sendMessage}
-                  panelState={panelState}
-                />
-              );
-            default:
-              return <div />;
-          }
-        })()}
-      </React.StrictMode>
-    );
-  } catch (error) {
-    console.error(error);
-  }
-};
+import * as StateManager from "./vscode/StateManager";
 
 const rootElement = document.getElementById("root");
+
 if (rootElement != undefined) {
-  reactRoot = createRoot(rootElement);
-  render();
+  const reactRoot = createRoot(rootElement);
+
+  const render = (panelState?: webviewApi.PanelState) => {
+    try {
+      reactRoot?.render(
+        <React.StrictMode>
+          {(() => {
+            switch (panelState?.type) {
+              case "chat":
+                return (
+                  <ChatPanelView
+                    sendMessage={sendMessage}
+                    panelState={panelState}
+                  />
+                );
+              case "diff":
+                return (
+                  <DiffPanelView
+                    sendMessage={sendMessage}
+                    panelState={panelState}
+                  />
+                );
+              default:
+                return <div />;
+            }
+          })()}
+        </React.StrictMode>
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  render(StateManager.getState());
+  StateManager.registerUpdateListener(render);
 }
-
-window.addEventListener("message", (rawMessage: unknown) => {
-  const event = webviewApi.incomingMessageSchema.parse(rawMessage);
-
-  const message = event.data;
-  if (message.type === "updateState" && render != null) {
-    render(message.state);
-  }
-});
