0f059e8f83aad8c290ca29be332cfcd4e5a6c942
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2023-02-01 14:39:40 -0500

    Cover getCompositeInput with tests

--- lib/extension/src/chat/getCompositeInput.test.ts
+++ lib/extension/src/chat/getCompositeInput.test.ts
@@ -1,7 +1,54 @@
 import { assert, describe, it } from "vitest";
+import { getCompositeInput } from "./getCompositeInput";
+import { getInput } from "./getInput";
 
 describe("getCompositeInput", () => {
-  it("should pass", () => {
-    assert.equal(true, true);
+  const firstInputData = { firstData: "irrelevant" };
+  const secondInputData = "irrelevant second input data";
+
+  it("should return composed result if all functions resolve", async () => {
+    const getInput = getCompositeInput({
+      first: createGetInputThatReturns(firstInputData),
+      second: createGetInputThatReturns(secondInputData),
+    });
+
+    const input = await getInput();
+    assert(
+      input.result === "success",
+      `Expected "success", received "${input.result}"`
+    );
+    assert.equal(input.data.get("first"), firstInputData);
+    assert.equal(input.data.get("second"), secondInputData);
+  });
+
+  it("should return an error if one composed function fails", async () => {
+    const errorMessage = "Something failed";
+
+    const getInput = getCompositeInput({
+      first: createGetInputThatReturns(firstInputData),
+      second: createGetInputThatFails(errorMessage),
+    });
+
+    const input = await getInput();
+    assert(
+      input.result === "unavailable",
+      `Expected "unavailable", received "${input.result}"`
+    );
+    assert.equal(input.message, errorMessage);
   });
 });
+
+function createGetInputThatReturns<DATA>(data: DATA): getInput<DATA> {
+  return async () => ({
+    result: "success",
+    data,
+  });
+}
+
+function createGetInputThatFails(message: string): getInput<unknown> {
+  return async () => ({
+    result: "unavailable",
+    type: "info",
+    message,
+  });
+}
