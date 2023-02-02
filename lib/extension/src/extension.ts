c15b5430d49978d5d9a9053a1c085954e8dfa68a
Lars Grammel <lars.grammel@gmail.com>
2023-02-02 09:23:23 +0100

    Move basicChatTemplate to BuiltInTemplates.

--- lib/extension/src/extension.ts
+++ lib/extension/src/extension.ts
@@ -6,6 +6,7 @@ import { DiagnoseErrorsConversationModel } from "./chat/DiagnoseErrorsConversati
 import { EditCodeConversationModel } from "./chat/EditCodeConversationModel";
 import { ExplainCodeConversationModel } from "./chat/ExplainCodeConversationModel";
 import { GenerateTestConversationModel } from "./chat/GenerateTestConversationModel";
+import { basicChatTemplate } from "./conversation-template/BuiltInTemplates";
 import { ConversationTemplateSchema } from "./conversation-template/ConversationTemplate";
 import { TemplateConversationFactory } from "./conversation-template/TemplateConversationFactory";
 import { DiffEditorManager } from "./diff/DiffEditorManager";
@@ -18,60 +19,7 @@ export const activate = async (context: vscode.ExtensionContext) => {
   });
 
   const basicChat = new TemplateConversationFactory({
-    template: ConversationTemplateSchema.parse({
-      id: "chat",
-      engineVersion: 0,
-      type: "basic-chat",
-      codicon: "comment-discussion",
-      prompt: {
-        sections: [
-          {
-            type: "lines",
-            title: "Instructions",
-            lines: [
-              "Continue the conversation below.",
-              "Pay special attention to the current developer request.",
-            ],
-          },
-          {
-            type: "lines",
-            title: "Current Request",
-            lines: ["Developer: ${lastMessage}"],
-          },
-          {
-            type: "optional-selected-code",
-            title: "Selected Code",
-          },
-          {
-            type: "conversation",
-            roles: {
-              bot: "Bot",
-              user: "Developer",
-            },
-          },
-          {
-            type: "lines",
-            title: "Task",
-            lines: [
-              "Write a response that continues the conversation.",
-              "Stay focused on current developer request.",
-              "Consider the possibility that there might not be a solution.",
-              "Ask for clarification if the message does not make sense or more input is needed.",
-              "Use the style of a documentation article.",
-              "Omit any links.",
-              "Include code snippets (using Markdown) and examples where appropriate.",
-            ],
-          },
-          {
-            type: "lines",
-            title: "Response",
-            lines: ["Bot:"],
-          },
-        ],
-        maxTokens: 1024,
-        stop: ["Bot:", "Developer:"],
-      },
-    }),
+    template: ConversationTemplateSchema.parse(basicChatTemplate),
   });
 
   const chatPanel = new ChatPanel({
