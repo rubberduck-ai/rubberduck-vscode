35431055d858371b467f06f99090fce55ef3eef6
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2023-02-10 11:49:26 -0500

    Extract <MessageExchange />

--- lib/webview/src/component/ExpandedConversationView.tsx
+++ lib/webview/src/component/ExpandedConversationView.tsx
@@ -1,8 +1,8 @@
 import { webviewApi } from "@rubberduck/common";
 import React from "react";
-import ReactMarkdown from "react-markdown";
 import { ChatInput } from "./ChatInput";
 import { ConversationHeader } from "./ConversationHeader";
+import { MessageExchange } from "./MessageExchange";
 
 export const ExpandedConversationView: React.FC<{
   conversation: webviewApi.Conversation;
@@ -26,65 +26,11 @@ export const ExpandedConversationView: React.FC<{
     <div className={`conversation expanded`}>
       <ConversationHeader conversation={conversation} />
 
-      <div className="detail">
-        {content.messages.map((message, i) => (
-          <div className={`message ${message.author}`} key={i}>
-            {message.author === "user" && message.content}
-            {message.author === "bot" && (
-              <ReactMarkdown>{message.content}</ReactMarkdown>
-            )}
-          </div>
-        ))}
-        {(() => {
-          const type = content.state.type;
-          switch (type) {
-            case "waitingForBotAnswer":
-              return (
-                <div className="message bot">
-                  {content.state.botAction ?? ""}
-                  <span className={"in-progress"} />
-                </div>
-              );
-            case "botAnswerStreaming":
-              return (
-                <div className="message bot">
-                  <ReactMarkdown>
-                    {content.state.partialAnswer ?? ""}
-                  </ReactMarkdown>
-                  <span className={"in-progress"} />
-                </div>
-              );
-            case "userCanReply":
-              return (
-                <ChatInput
-                  placeholder={
-                    content.state.responsePlaceholder ??
-                    content.messages.length > 0
-                      ? "Reply…"
-                      : "Ask…"
-                  }
-                  onSend={onSendMessage}
-                />
-              );
-            case "error":
-              return (
-                <div key={"error"} className={"message bot error"}>
-                  <span className={"error-message"}>
-                    Error: {content.state.errorMessage}
-                  </span>
-                  <span className={"error-retry"} onClick={onClickRetry}>
-                    <i className="codicon codicon-debug-restart inline" />
-                    <span style={{ marginLeft: "5px" }}>Retry</span>
-                  </span>
-                </div>
-              );
-            default: {
-              const exhaustiveCheck: never = type;
-              throw new Error(`unsupported type: ${exhaustiveCheck}`);
-            }
-          }
-        })()}
-      </div>
+      <MessageExchange
+        content={content}
+        onClickRetry={onClickRetry}
+        onSendMessage={onSendMessage}
+      />
 
       <div className="footer">
         <span className="action-panel">
---
+++ lib/webview/src/component/MessageExchange.tsx
@@ -0,0 +1,76 @@
+import { MessageExchangeContent } from "@rubberduck/common/build/webview-api";
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import { ChatInput } from "./ChatInput";
+
+export function MessageExchange({
+  content,
+  onClickRetry,
+  onSendMessage,
+}: {
+  content: MessageExchangeContent;
+  onSendMessage: (message: string) => void;
+  onClickRetry: () => void;
+}) {
+  return (
+    <div className="message-exchange">
+      {content.messages.map((message, i) => (
+        <div className={`message ${message.author}`} key={i}>
+          {message.author === "user" && message.content}
+          {message.author === "bot" && (
+            <ReactMarkdown>{message.content}</ReactMarkdown>
+          )}
+        </div>
+      ))}
+      {(() => {
+        const type = content.state.type;
+        switch (type) {
+          case "waitingForBotAnswer":
+            return (
+              <div className="message bot">
+                {content.state.botAction ?? ""}
+                <span className={"in-progress"} />
+              </div>
+            );
+          case "botAnswerStreaming":
+            return (
+              <div className="message bot">
+                <ReactMarkdown>
+                  {content.state.partialAnswer ?? ""}
+                </ReactMarkdown>
+                <span className={"in-progress"} />
+              </div>
+            );
+          case "userCanReply":
+            return (
+              <ChatInput
+                placeholder={
+                  content.state.responsePlaceholder ??
+                  content.messages.length > 0
+                    ? "Reply…"
+                    : "Ask…"
+                }
+                onSend={onSendMessage}
+              />
+            );
+          case "error":
+            return (
+              <div key={"error"} className={"message bot error"}>
+                <span className={"error-message"}>
+                  Error: {content.state.errorMessage}
+                </span>
+                <span className={"error-retry"} onClick={onClickRetry}>
+                  <i className="codicon codicon-debug-restart inline" />
+                  <span style={{ marginLeft: "5px" }}>Retry</span>
+                </span>
+              </div>
+            );
+          default: {
+            const exhaustiveCheck: never = type;
+            throw new Error(`unsupported type: ${exhaustiveCheck}`);
+          }
+        }
+      })()}
+    </div>
+  );
+}
