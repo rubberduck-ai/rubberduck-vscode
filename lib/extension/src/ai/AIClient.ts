333e40f21b7d9aa083e1217c9716636374ee3d52
restlessronin <88921269+restlessronin@users.noreply.github.com>
2023-11-28 11:56:22 +0530

    added support for 1106 models

--- lib/extension/src/ai/AIClient.ts
+++ lib/extension/src/ai/AIClient.ts
@@ -31,8 +31,10 @@ function getModel() {
     .enum([
       "gpt-4",
       "gpt-4-32k",
+      "gpt-4-1106-preview",
       "gpt-3.5-turbo",
       "gpt-3.5-turbo-16k",
+      "gpt-3.5-turbo-1106",
       "llama.cpp",
     ])
     .parse(vscode.workspace.getConfiguration("rubberduck").get("model"));
