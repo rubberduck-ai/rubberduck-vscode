67864041904b43ff989d2b45d58e3d5fb5459e5a
Lars Grammel <lars.grammel@gmail.com>
2024-01-28 13:30:47 +0100

    Update to ModelFusion v0.134.0

--- lib/extension/src/ai/AIClient.ts
+++ lib/extension/src/ai/AIClient.ts
@@ -28,9 +28,11 @@ function getModel() {
       "gpt-4",
       "gpt-4-32k",
       "gpt-4-1106-preview",
+      "gpt-4-0125-preview",
       "gpt-3.5-turbo",
       "gpt-3.5-turbo-16k",
       "gpt-3.5-turbo-1106",
+      "gpt-3.5-turbo-0125",
       "llama.cpp",
     ])
     .parse(vscode.workspace.getConfiguration("rubberduck").get("model"));
@@ -135,7 +137,7 @@ export class AIClient {
         type: "success" as const,
         embedding,
         totalTokenCount: (rawResponse as OpenAITextEmbeddingResponse).usage
-          .total_tokens,
+          ?.total_tokens,
       };
     } catch (error: any) {
       console.log(error);
